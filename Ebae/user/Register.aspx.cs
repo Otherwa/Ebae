e49f5a31f27b52f8568df3d43b9eb65507026528
SuperShotRETRO <SARIKAMADHAVI26@GMAIL.COM>
2024-04-04 21:58:26 +0530

    Registration

--- Ebae/user/Register.aspx.cs
+++ Ebae/user/Register.aspx.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Ebae.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +14,39 @@ namespace Ebae.user
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+        protected void register_Click(object sender, EventArgs e)
+        {
+            string email = emailTextBox.Text;
+            string name = nameTextBox.Text;
+            string password = passwordTextBox1.Text;
+            string conPassword = passwordTextBox2.Text;
+            string result;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(conPassword))
+            {
+                Label1.Text = "All fields are required.";
+                return;
+            }
+
+            if (Equals(password,conPassword))
+            {
+                Response.Write($"<script>console.log('clicked');</script>");
+                User user = new User(email,password,name);
+                result = user.Register();
+                Label1.Text = result;
+            }
+            else
+            {
+                Label1.Text = "Passwords do not match.";
+            }
         }
     }
 }
