ea764181966be1cca05e6ba53da839fe9a352079
Pratham24082002 <126474691+Pratham24082002@users.noreply.github.com>
2024-04-06 20:05:19 +0530

    Login Validation

--- Ebae/user/Login.aspx.cs
+++ Ebae/user/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ebae.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,31 @@ namespace Ebae.user
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                Label3.Text = "Both username and password are required.";
+                return;
+            }
+
+            User user = new User(username, password);
+            bool isLoggedIn = user.Login();
+
+            if (isLoggedIn)
+            {
+                // Redirect to the home page or any other desired page upon successful login
+                Response.Redirect("../Default.aspx");
+            }
+            else
+            {
+                Label3.Text = "Invalid username or password. Please try again.";
+            }
         }
     }
 }
